007efd41c1470943deb66e11e110b27b42a90cc3
Akulakhan <zacharyallen@gmx.com>
2019-08-25 20:50:46 -0700

    configurable host location

--- src/main/java/demo/controller/HelloController.java
+++ src/main/java/demo/controller/HelloController.java
@@ -1,11 +1,25 @@
 package demo.controller;
 
 import demo.valueobject.WelcomeMessage;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloController {
+
+    @Value("${service.host}")
+    private String host;
+
+    @RequestMapping("/")
+    public ModelAndView index() {
+        final ModelAndView modelAndView = new ModelAndView("index");
+
+        modelAndView.addObject("host", host);
+
+        return modelAndView;
+    }
 
     @RequestMapping("/hello")
     public WelcomeMessage hello() {
--- src/main/java/demo/controller/LobbyController.java
+++ src/main/java/demo/controller/LobbyController.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@RestController()
+@RestController
 public class LobbyController {
 
     private final ILobbyService lobbyService;
